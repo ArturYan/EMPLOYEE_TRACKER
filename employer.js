3cd42e4051779a41a9ff2d60a5cba6f614cdef66
Artur Simonyan <artursimonyank@gmail.com>
2020-06-25 18:36:31 -0700

    did minor change

--- employer.js
+++ employer.js
@@ -1,7 +1,7 @@
-var inquirer = require("inquirer")
+const inquirer = require("inquirer")
 //   * Add departments, roles, employees
-var mysql = require("mysql")
-var connection = mysql.createConnection({
+const mysql = require("mysql")
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
